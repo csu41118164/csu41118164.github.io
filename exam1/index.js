7620960eae4ffc4f1a7024c838f9a0a6992ea317
csu41118164 <148747274+csu41118164@users.noreply.github.com>
2023-12-25 15:08:02 +0800

    Update index.js

--- exam1/index.js
+++ exam1/index.js
@@ -1,5 +1,15 @@
 function onclick1(){
   var style = document.getElementById("number").style;
   style.color = "red";
-  style.fontFamily = "Times New Roman";
-}
+  style.fontFamily = "Arial";
+};
+function onclick2(){
+  var style = document.getElementById("image").style;
+  style.borderRadius = 0.5;
+  style.borderStyle = "solid";
+  style.borderWidth = "7px";
+  style.borderColor = "red";
+};
+function onclick3(){
+  document.getElementById("image").src = "https://raw.githubusercontent.com/csu41118164/csu41118164.github.io/main/exam1/P_20231225_150354.jpg";
+};
