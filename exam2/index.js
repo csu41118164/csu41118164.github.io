499b89181ff5481b68379e954db887c1e9ed9d8a
csu41118164 <148747274+csu41118164@users.noreply.github.com>
2023-12-25 15:25:12 +0800

    Create index.js

---
+++ exam2/index.js
@@ -0,0 +1,16 @@
+function onclick1(){
+  var number = $("#number");
+  number.css("color") = "red";
+  number.css("font-family") = "Arial";
+};
+function onclick2(){
+  var image = $("#image");
+  image.css("border-radius") = "0.5";
+  image.css("border-style") = "solid";
+  image.css("border-width") = "7px";
+  image.css("border-color") = "red";
+  image.css("clip-path") = "circle()";
+};
+function onclick3(){
+  $("#image").attr("src","https://raw.githubusercontent.com/csu41118164/csu41118164.github.io/main/exam1/P_20231225_150354.jpg");
+};
