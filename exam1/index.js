585d1274724a57e620e65eb85184999b53450645
csu41118164 <148747274+csu41118164@users.noreply.github.com>
2023-12-25 15:16:51 +0800

    Update index.js

--- exam1/index.js
+++ exam1/index.js
@@ -9,7 +9,7 @@ function onclick2(){
   style.borderStyle = "solid";
   style.borderWidth = "7px";
   style.borderColor = "red";
-  style.clipPath: "circle()";
+  style.clipPath= "circle()";
 };
 function onclick3(){
   document.getElementById("image").src = "https://raw.githubusercontent.com/csu41118164/csu41118164.github.io/main/exam1/P_20231225_150354.jpg";
